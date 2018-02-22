d28af1760f439bea474ba1b7930fa930755c341b
ysaibhargav <bhargavcoolboy@gmail.com>
2018-02-21 22:22:08 -0500

    SBGAN initial version that trains without errors

---
+++ bayes_mnist.py
@@ -0,0 +1,97 @@
+# Let your BUILD target depend on "//tensorflow/python/debug:debug_py"
+# (You don't need to worry about the BUILD dependency if you are using a pip
+#  install of open-source TensorFlow.)
+from tensorflow.python import debug as tf_debug
+
+
+import tensorflow as tf 
+import numpy as np
+import os
+from skimage.io import imsave
+from collections import namedtuple
+
+from sbgan import SBGAN
+
+fc = tf.contrib.layers.fully_connected
+Hook = namedtuple("Hook", ["n", "f"])
+
+
+class _config(object):
+    def __init__(self):
+        self.x_batch_size = 256
+        self.z_batch_size = 256
+        self.z_dims = 100
+        self.z_std = 1
+        self.num_epochs = 100 
+        self.prior_std = 1
+        self.step_size = 0.001
+
+
+def hook_arg_filter(*_args):
+    def hook_decorator(f):
+        def func_wrapper(*args, **kwargs):
+            return f(*[kwargs[arg] for arg in _args])
+        return func_wrapper
+    return hook_decorator
+
+
+@hook_arg_filter("g_z", "epoch")
+def show_result(batch_res, fname, grid_size=(8, 8), grid_pad=5):
+    if not os.path.exists("out"):
+        os.mkdir("out")
+    if not os.path.exists(os.path.join("out", "mnist")):
+        os.mkdir(os.path.join("out", "mnist"))
+
+    img_height = img_width = 28
+    batch_res = 0.5 * batch_res.reshape((batch_res.shape[0], \
+            img_height, img_width)) + 0.5
+    img_h, img_w = batch_res.shape[1], batch_res.shape[2]
+    grid_h = img_h * grid_size[0] + grid_pad * (grid_size[0] - 1)
+    grid_w = img_w * grid_size[1] + grid_pad * (grid_size[1] - 1)
+    img_grid = np.zeros((grid_h, grid_w), dtype=np.uint8)
+    for i, res in enumerate(batch_res):
+        if i >= grid_size[0] * grid_size[1]:
+            break
+        img = (res) * 255.
+        img = img.astype(np.uint8)
+        row = (i // grid_size[0]) * (img_h + grid_pad)
+        col = (i % grid_size[1]) * (img_w + grid_pad)
+        img_grid[row:row + img_h, col:col + img_w] = img
+    imsave(os.path.join("out", "mnist", "%d.png"%fname), img_grid)
+
+
+def generator(z, scope="generator"):
+    with tf.variable_scope(scope):
+        h1 = fc(z, 150, reuse = tf.AUTO_REUSE, scope = "h1")
+        h2 = fc(h1, 300, reuse = tf.AUTO_REUSE, scope = "h2")
+        h3 = fc(h2, 784, activation_fn = None, \
+                reuse = tf.AUTO_REUSE, scope = "h3")
+        o = tf.nn.tanh(h3)
+            
+        return o
+
+# TODO: dropout
+def discriminator(x, scope="discriminator"):
+    with tf.variable_scope(scope):
+        h1 = fc(x, 200, reuse = tf.AUTO_REUSE, scope = "h1")
+        h2 = fc(h1, 150, reuse = tf.AUTO_REUSE, scope = "h2")
+        h3 = fc(h2, 1, activation_fn = None, \
+                reuse = tf.AUTO_REUSE, scope = "h3")
+        o = tf.nn.sigmoid(h3)
+
+        return o
+
+
+mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+real_data = mnist.train.images
+
+# copy by reference?
+# TODO: read about Adam
+config = _config()
+hook1 = Hook(1, show_result)
+
+m = SBGAN(generator, discriminator)
+# TODO: cleanup code by placing session creation inside .train()
+sess = tf.Session()
+#sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+m.train(sess, real_data, config, hooks = [hook1])
--- sbgan.py
+++ sbgan.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import pdb
+from functools import partial
 
 
 class SBGAN(object):
@@ -6,27 +8,56 @@ class SBGAN(object):
             generator,
             discriminator,
             n_g=5,
-            n_d=1
+            n_d=1,
             kernel="rbf"):
-        # TODO: property for tf scope argument
-        self.generator = generator
-        self.discriminator = discriminator 
+        self.generator = lambda scope: partial(generator, scope=scope) 
+        self.discriminator = lambda scope: partial(discriminator, scope=scope)
         self.n_g = n_g
         self.n_d = n_d
         self.kernel = self._kernel(kernel)
 
 
+    # TODO: adaptively change bandwidth (page 6, section 5)
     def _kernel(self,
             kernel):
-        pass
+        def _rbf(x1, x2):
+            h = 1
+            
+            def _to_tf_vec(x):
+                x = [tf.reshape(_x, [-1]) for _x in x]
+                return tf.concat(x, 0)
+
+            if type(x1).__name__ == 'list':
+                x1 = _to_tf_vec(x1)
+            if type(x2).__name__ == 'list':
+                x2 = _to_tf_vec(x2)
+
+            return tf.exp(-tf.reduce_sum((x1-x2)*(x1-x2))/h)
+            
+        if kernel == "rbf":
+            return _rbf
 
 
     def _stein_phi_star(self,
             theta,
             particles,
-            posterior,
-            config):
-        pass
+            posterior):
+        num_param = len(theta)
+        phi_star = [0 for _ in range(num_param)]
+
+        for i, particle in enumerate(particles):
+            grad_log_post = tf.gradients(posterior[i], particle)
+            grad_kernel = tf.gradients(self.kernel(particle, theta), \
+                    particle)
+
+            for j in range(num_param):
+                phi_star[j] += self.kernel(particle, theta)*grad_log_post[j] + \
+                        grad_kernel[j]
+
+        for j in range(num_param):
+            phi_star[j] /= len(particles)
+
+        return phi_star
 
 
     def _data_handler(self,
@@ -45,21 +76,27 @@ class SBGAN(object):
         z = tf.random_normal([2, self.n_g, config.z_batch_size, \
                 config.z_dims], stddev = config.z_std)
 
-        return x, z
+        return x, z, iterator
 
 
     def _prior(self,
-            params):
-        pass
+            params_group,
+            config):
+        prior_loss = 0
+        for param in params_group:
+            param /= config.prior_std
+            prior_loss -= tf.reduce_mean(tf.multiply(param, param))
+
+        return prior_loss
 
 
     def train(self,
             sess,
             real_data,
             config,
-            g_scope = "generator",
-            d_scope = "discriminator",
-            hooks = None):
+            g_scope="generator",
+            d_scope="discriminator",
+            hooks=None):
         """
         config: should have the following properties
                 x_batch_size
@@ -68,7 +105,7 @@ class SBGAN(object):
                 z_std
                 num_epochs
                 num_svgd_iterations
-                learning_rate 
+                step_size 
                 prior_std
         """
         def _get_var(scope):
@@ -78,52 +115,52 @@ class SBGAN(object):
         def _flatten(main_list):
             return [item for sub_list in main_list for item in sub_list]
 
-        x, z = self._data_handler(config, real_data)
+        x, z, iterator = self._data_handler(config, real_data)
         eps = tf.placeholder(dtype=tf.float32)
         
         # network initialisation
-        generators = [self.generator(g_scope+"_%d"%i) for i in range(n_g)]
-        discriminators = [self.discriminator(d_scope+"_%d"%i) for i in range(n_d)]
-
-        var_g = [_get_var(g_scope+"_%d"%i) for i in range(n_g)]
-        var_d = [_get_var(d_scope+"_%d"%i) for i in range(n_d)]
-
-        # priors
-        prior_g = [self._prior(_var_g) for _var_g in var_g]
-        prior_d = [self._prior(_var_d) for _var_d in var_d] 
+        # TODO: initialize points from the prior (page 6, section 5)
+        generators = [self.generator(g_scope+"_%d"%i) for i in range(self.n_g)]
+        discriminators = [self.discriminator(d_scope+"_%d"%i) for i in range(self.n_d)]
 
         # posteriors
-        post_g = [None for _ in range(n_g)]
-        for i in range(n_g):
-            post_g[i] = 0 
-            for j in range(n_d):
+        post_g = [0. for _ in range(self.n_g)]
+        for i in range(self.n_g):
+            for j in range(self.n_d):
                 post_g[i] += tf.reduce_mean(\
                         tf.log(discriminators[j](generators[i](z[0][i]))))
-            post_g[i] /= n_d
-            post_g[i] += prior_g[i]
+            post_g[i] /= self.n_d
 
-        post_d = [None for _ in range(n_d)]
-        for i in range(n_d):
-            post_d[i] = 0 
+        post_d = [0. for _ in range(self.n_d)]
+        for i in range(self.n_d):
             post_d[i] += tf.reduce_mean(tf.log(discriminators[i](x[i])))
-            for j in range(n_g):
+            for j in range(self.n_g):
                 post_d[i] += tf.reduce_mean(\
                         tf.log(1.-discriminators[i](generators[j](z[1][j]))))
-            post_d[i] /= n_d
-            post_d[i] += prior_d[i]
+            post_d[i] /= self.n_g
+
+        var_g = [_get_var(g_scope+"_%d"%i) for i in range(self.n_g)]
+        var_d = [_get_var(d_scope+"_%d"%i) for i in range(self.n_d)]
+
+        # priors
+        prior_g = [self._prior(_var_g, config) for _var_g in var_g]
+        prior_d = [self._prior(_var_d, config) for _var_d in var_d] 
+
+        for i in range(self.n_g): post_g[i] += prior_g[i]
+        for i in range(self.n_d): post_d[i] += prior_d[i]
 
         # train steps
         # TODO: annealing
-        g_phi_star = [self._stein_phi_star(var_g[i], var_g, post_g[i], config)\
-                for i in range(n_g)]
-        g_train_steps = [[tf.assign(_var_g, _var_g+eps*g_phi_star[i]) for i, _var_g \
-                in enumerate(var_g[j])] for j in range(ng)]
+        g_phi_star = [self._stein_phi_star(var_g[i], var_g, post_g) \
+                for i in range(self.n_g)]
+        g_train_steps = [[tf.assign(_var_g, _var_g+eps*g_phi_star[j][i]) for i, _var_g \
+                in enumerate(var_g[j])] for j in range(self.n_g)]
         g_train_steps = _flatten(g_train_steps) 
 
-        d_phi_star = [self._stein_phi_star(var_d[i], var_d, post_d[i], config)\
-                for i in range(n_d)]
-        d_train_steps = [[tf.assign(_var_d, _var_d+eps*d_phi_star[i]) for i, _var_d \
-                in enumerate(var_d[j])] for j in range(nd)]
+        d_phi_star = [self._stein_phi_star(var_d[i], var_d, post_d) \
+                for i in range(self.n_d)]
+        d_train_steps = [[tf.assign(_var_d, _var_d+eps*d_phi_star[j][i]) for i, _var_d \
+                in enumerate(var_d[j])] for j in range(self.n_d)]
         d_train_steps = _flatten(d_train_steps) 
 
 
@@ -132,6 +169,7 @@ class SBGAN(object):
 
         # run
         for epoch in range(config.num_epochs): 
+            print epoch
             sess.run(iterator.initializer)            
 
             while True:
@@ -141,7 +179,5 @@ class SBGAN(object):
 
                 except tf.errors.OutOfRangeError:
                     break
-
-
 
 
