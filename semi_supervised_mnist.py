8183386154cf842a21c36a6b84007c65ce8e87d5
Sarthak <sarthakg@andrew.cmu.edu>
2018-04-12 18:59:47 -0400

    Saving images to output dir

--- semi_supervised_mnist.py
+++ semi_supervised_mnist.py
@@ -109,7 +109,11 @@ def show_result(batch_res, fname, grid_size=(8, 8), grid_pad=5):
 		row = (i // grid_size[0]) * (img_h + grid_pad)
 		col = (i % grid_size[1]) * (img_w + grid_pad)
 		img_grid[row:row + img_h, col:col + img_w] = img
-	imsave(os.path.join("out", "b-mnist", "%s.png"%str(fname)), img_grid)
+        folder_path = os.path.join(config.output_dir, 'b-mnist')
+        if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+        file_path = os.path.join(folder_path, "%s.png"%str(fname))
+	imsave(os.path.join(file_path, img_grid)
 
 
 def generator(z, scope="generator"):
