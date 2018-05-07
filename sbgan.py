small changes, fix in semi supervised scaling factor

sbgan.py
@@ -160,6 +160,7 @@ class SBGAN(object):
                     config.x_batch_size)
             d_labels_classes = tf.concat(values=[tf.constant(0., shape=[config.n_supervised, 
                 1]), data.ys], axis=1)
+            scaling_factor = 1. * config.supervised_scaling * config.x_batch_size / config.n_supervised
             for i in range(self.n_d):
                 'real samples'
 
@@ -170,7 +171,7 @@ class SBGAN(object):
 
             
                 'semi supervised'
-                post_d[i] -= config.supervised_scaling * tf.reduce_sum(
+                post_d[i] -= scaling_factor * tf.reduce_sum(
                     tf.nn.softmax_cross_entropy_with_logits(
                         labels=d_labels_classes,
                         logits=discriminators[i](data.xs)